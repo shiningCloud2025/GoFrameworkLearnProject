94cc3c3ca4e35cae5e7a9b8aa97b9c9fd766801b
shiningCloud2025 <shining_cloud2025@163.com>
2026-02-11 20:08:27 +0800

    GORM-where

---
+++ GormStart/chapter06/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type User struct {
+	ID           uint           // Standard field for the primary key
+	Name         string         // A regular string field
+	Email        *string        // A pointer to a string, allowing for null values
+	Age          uint8          // An unsigned 8-bit integer
+	Birthday     *time.Time     // A pointer to time.Time, can be null
+	MemberNumber sql.NullString // Uses sql.NullString to handle nullable strings
+	ActivatedAt  sql.NullTime   // Uses sql.NullTime for nullable time fields
+	CreatedAt    time.Time      // Automatically managed by GORM for creation time
+	UpdatedAt    time.Time      // Automatically managed by GORM for update time
+	ignored      string         // fields that aren't exported are ignored
+}
+
+func main() {
+	// 参考 https://github.com/go-sql-driver/mysql#dsn-data-source-name 获取详情
+	dsn := "root:mysql~!@#$%^&*()_+@tcp(117.50.184.138:37210)/zyh_gorm_test?charset=utf8mb4&parseTime=True&loc=Local"
+
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second, // Slow SQL threshold
+			LogLevel:                  logger.Info, // Log level
+			IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
+			ParameterizedQueries:      true,        // Don't include params in the SQL log
+			Colorful:                  true,        // Disable color
+		},
+	)
+
+	// Globally mode
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	// 通过where查询
+	//var user User
+	var users []User
+	//db.Where("name=?", "zyh").First(&user)
+	//db.Where(&User{Name: "zyh"}).First(&user)
+	//db.Where(&User{Name: "zyh"}).Find(&users)
+	// map会包含0值第二种不会
+	db.Where(map[string]interface{}{"Myname": "zyh"}).Find(&users)
+	for _, user := range users {
+		fmt.Println(user.ID)
+	}
+
+	// 查询方式条件有三种:1.string 2.struct 3.map
+	// 优先级 2 3 1
+
+}
